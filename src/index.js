1c9ae4d44e6155ecb045caf02592a6795b899a1a
makibishi <makibishi0212@gmail.com>
2018-10-20 14:18:57 +0900

    introduce paperjs

--- src/index.js
+++ src/index.js
@@ -1,31 +1,70 @@
-import Phaser from "phaser";
+import paper from 'paper';
 
-import constants from "./config/constants";
-import GameScene from "./scenes/game";
+const canvas = document.getElementById('myCanvas');
 
-const config = {
-  type: Phaser.AUTO,
-  width: constants.WIDTH,
-  height: constants.HEIGHT,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: {
-        y: 200
-      },
-      debug: false
+paper.install(window);
+paper.setup(canvas);
+
+class HyperRect {
+  constructor(topLeft, size, worldLength, depth, color = 'red') {
+    this.origin = new paper.Path.Rectangle(topLeft, size);
+    this.origin.fillColor = color;
+    this.worldLength = worldLength;
+    this.depth = depth;
+    this.unitLength = worldLength / (2 ** (depth - 1));
+    this.mortonOrder = null;
+  }
+
+  get x() {
+    return this.origin.position.x;
+  }
+
+  set x(x) {
+    this.origin.position.x = x;
+    this.computeRectMortonOrder();
+  }
+
+  get y() {
+    return this.origin.position.y;
+  }
+
+  set y(y) {
+    this.origin.position.y = y;
+    this.computeRectMortonOrder();
+  }
+
+  intersects(hyperRect) {
+    if (this.origin.bounds.intersects(hyperRect.origin.bounds)) {
+      return true;
     }
-  },
-  scene: [GameScene]
-};
 
-// eslint-disable-next-line no-new
-new Phaser.Game(config);
+    return false;
+  }
 
-if (module.hot) {
-  module.hot.accept(() => {});
+  computeRectMortonOrder() {
+    const { topLeft, bottomRight } = this.origin.bounds;
+    this.mortonOrder = null;
+  }
 
-  module.hot.dispose(() => {
-    window.location.reload();
-  });
+  computePointMortonOrder(point) {
+    const horizontal = Math.floor(point.x / this.unitLength);
+    const vertical = Math.floor(point.y / this.unitLength);
+    
+    const posToOrder = (vertical, horizontal) => {
+      let tmpOrder = 0;
+      for (let digit = this.depth; digit >= 0; digit -= 1) {
+
+      }
+    }
+  }
 }
+
+const rect = new HyperRect(new paper.Point(10, 10), new paper.Size(20, 20), paper.view.size.width, 4, 'green');
+const rect2 = new HyperRect(new paper.Point(100, 10), new paper.Size(150, 150), paper.view.size.width, 4, 'red');
+
+paper.view.onFrame = () => {
+  rect.x += 0.5;
+  if (rect.intersects(rect2)) {
+    console.log('oh hit');
+  }
+};
